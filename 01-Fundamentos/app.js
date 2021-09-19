4c881009e905f9b225fc5bbd3df8d36dfb3afc09
Lorena <lorena.perez@gmail.com>
2021-09-19 16:00:46 +0200

    console

--- 01-Fundamentos/app.js
+++ 01-Fundamentos/app.js
@@ -1,1 +1,21 @@
-console.log(console.log("Hola mundo"));
+// console.log(console.log("Hola mundo"));
+
+//variable:
+// = asigna un valor a la variable
+let a = 10,
+  b = 10,
+  c = 10;
+let d = 10;
+let x = a + b;
+
+console.log(x);
+console.warn(x);
+console.error(x);
+
+console.log("a", a);
+console.log("b", b);
+console.log("c", c);
+
+console.log({ a });
+console.log({ b });
+console.log({ c });
