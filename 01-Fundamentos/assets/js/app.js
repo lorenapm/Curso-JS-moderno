712d816c4d87a1132af742eeabf0ced801e6f73d
Lorena <lorena.perez@gmail.com>
2021-09-19 16:40:09 +0200

    order scripts

---
+++ 01-Fundamentos/assets/js/app.js
@@ -0,0 +1,31 @@
+// console.log(console.log("Hola mundo"));
+
+//variable:
+// = asigna un valor a la variable
+let a = 10,
+  b = 10,
+  c = "Hola ";
+let d = "Spiderman";
+let x = a + b;
+
+const greeting = c + d;
+
+// console.log(x);
+// console.warn(x);
+// console.error(x);
+
+// console.log("a", a);
+// console.log("b", b);
+// console.log("c", c);
+
+// console.log({ a });
+// console.log({ b });
+// console.log({ c });
+
+c = "Hola de nuevo";
+
+// console.table({ a, b, c, d, x });
+
+// console.log(greeting);
+
+const myName = "Lorena";
