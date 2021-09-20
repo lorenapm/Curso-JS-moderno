adba6a5ec07cd8f8f1ad494e4fbee819331d3a56
Lorena <lorena.perez@gmail.com>
2021-09-20 09:55:01 +0200

    function return

--- 01-Fundamentos/assets/js/functions.js
+++ 01-Fundamentos/assets/js/functions.js
@@ -16,7 +16,26 @@ const greetingArrow2 = (user) => {
   console.log("Hola " + user);
 };
 
-greeting("Lorena", 50, "Cipolletti");
-greeting2("Lorena");
-greetingArrow();
-greetingArrow2("Lorena");
+// greeting("Lorena", 50, "Cipolletti");
+// greeting2("Lorena");
+// greetingArrow();
+// greetingArrow2("Lorena");
+
+function add(a, b) {
+  return a + b;
+}
+
+const add2 = (a, b) => {
+  return a + b;
+};
+
+//cuando solo tengo 1 línea de código y es el return puedo simplificar
+const add3 = (a, b) => a + b;
+
+function getRamdom() {
+  return Math.random();
+}
+
+const getRamdom2 = () => Math.random();
+
+console.log(getRamdom2());
