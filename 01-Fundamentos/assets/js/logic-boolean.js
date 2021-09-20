9b02096277b8466877f9017410d5de97a7283b4a
Lorena <lorena.perez@gmail.com>
2021-09-20 13:37:20 +0200

    and, or, asignaciones

--- 01-Fundamentos/assets/js/logic-boolean.js
+++ 01-Fundamentos/assets/js/logic-boolean.js
@@ -35,3 +35,16 @@ console.log(returnTrue() || returnFalse());
 console.log("4 condiciones", true || true || true || false); // true (porque 1 es V)
 
 console.warn("Asignaciones");
+
+const isUndefined = undefined;
+const isNull = null;
+const isFalse = false;
+
+const a1 = true && "Hola mundo" && 150; // si todos tienen valor, retorna el último
+const a2 = "Hola" && "Mundo" && true;
+const a3 = isFalse || "ya no soy false"; // retorna la segunda porque el 1 tiene valor
+const a4 = isFalse || isUndefined || isNull || "ya no soy false"; // ya no soy falso
+const a5 = isFalse || isUndefined || isNull || "ya no soy false" || true; // ya no soy falso porque no llega al true final
+const a6 = isFalse || isUndefined || returnTrue() || "ya no soy false" || true; // true, porque true es un valor permiido de returnTrue()
+
+console.log(a1, a2, a3, a4, a5, a6);
