fdc03e2db4105d6c203907e6daaa6e72a8309a56
Lorena <lorena.perez@gmail.com>
2021-09-21 12:24:10 +0200

    switch

---
+++ 01-Fundamentos/assets/js/switch.js
@@ -0,0 +1,20 @@
+const day = 6; // domingo
+
+switch (day) {
+  case 0:
+    console.log("Domingo");
+    break;
+  case 1:
+    console.log("Lunes");
+    break;
+  case 2:
+    console.log("Martes");
+    break;
+  case 3:
+    console.log("Miércoles");
+    break;
+
+  default:
+    console.log("No es de domingo a miércoles");
+    break;
+}
